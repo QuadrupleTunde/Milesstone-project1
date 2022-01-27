088f99d10fa26693b95fc6107ec69b01009656f0
OLATUNDE <olatundequadril@gmail.com>
2022-01-27 00:07:03 -0500

    in-progress

---
+++ script.js
@@ -0,0 +1,25 @@
+playGame = () =>{
+    let p1Score = 0;
+    let p2Socre = 0;
+
+    startGame = () => {
+        let startButton = document.getElementsByClassName(".intro-page button");
+        let  introScreen = document.querySelector(".intro-pager");
+        let matchScreen = document.querySelector(".intro-page");
+        startButton.addEventListener("click", ()=>{
+            console.log(startButton)
+
+        // introPage.classList.add("fadeOut");
+        // optionPage.classList.add("fadeIn");
+
+        })
+
+
+
+
+    };
+
+
+    startGame ()
+}
+playGame()
